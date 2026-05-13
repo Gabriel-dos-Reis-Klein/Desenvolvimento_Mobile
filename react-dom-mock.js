8512bce6660c4d40def37389860e7cc9a3ef1030
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-05-13 10:34:28 -0300

    :package: build: resolver problemas de chamadas de pacotes react-dom do gluestacks

---
+++ react-dom-mock.js
@@ -0,0 +1,8 @@
+// Arquivo de segurança para interceptar chamadas web no Android/iOS
+module.exports = {
+  render: () => {},
+  hydrate: () => {},
+  createPortal: () => null,
+  findDOMNode: () => null,
+  version: '0.0.0',
+};
