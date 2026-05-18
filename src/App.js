68ca6b6c2f8a86ba16fd25db90e5662bcb1a5002
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-05-18 19:07:29 -0300

    :broom: cleanup(App): remover código desnecessário do App.js

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Provider as PaperProvider } from 'react-native-paper';
+
+import Login from './screens/Login';
+import Home from './screens/Home';
+import Details from './screens/Details';
+import Register from './screens/Register';
+import Clientes from './screens/Clientes'
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const PedidosStack = createStackNavigator();
+
+function MainTabs() {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen 
+        name="Home" 
+        component={Home} 
+        options={{ title: 'Pedidos' }} 
+      />
+      <Tab.Screen name="Clientes" component={Clientes} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="MainHome" component={MainTabs} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
