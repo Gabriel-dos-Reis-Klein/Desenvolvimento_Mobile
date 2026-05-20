2761e31f7e5e1ff42d6c1f34601cba2108798235
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-05-20 08:41:46 -0300

    :sparkles: feat(hooks): adicionar hook para carregar fontes

---
+++ src/hooks/useAppFonts.js
@@ -0,0 +1,31 @@
+import { useFonts } from 'expo-font';
+
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+
+} from '@expo-google-fonts/poppins';
+
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+
+} from '@expo-google-fonts/roboto';
+
+
+export function useAppFonts() {
+
+  return useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+}
