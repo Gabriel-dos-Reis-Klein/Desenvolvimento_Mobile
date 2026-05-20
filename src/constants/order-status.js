d2b12ba2b56dc04636a2e20faa042ae7e0c85a5d
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-05-20 08:56:13 -0300

    :sparkles: feat(theme): criar constante de cores para ordens de pedidos

--- src/constants/order-status.js
+++ src/constants/order-status.js
@@ -1,11 +1,12 @@
+// TODO: Requisitar alteração no BACK para trocar 
+// constantes pelos seguintes valores:
+//
+// - Produção (PRODUCTION)
+// - Pronto   (DONE)
+// - Entregue (DELIVERED)
+
 export const ORDER_STATUS = {
   WAITING: 'Aguardando',
   PRODUCTION: 'Em andamento',
   DELIVERED: 'Entregue',
-};
-
-export const ORDER_STATUS_COLORS = {
-  [ORDER_STATUS.WAITING]: '#FFB800',
-  [ORDER_STATUS.PRODUCTION]: '#FF00FF',
-  [ORDER_STATUS.DELIVERED]: '#00D1FF',
 };
