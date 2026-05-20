410f66b1004097ad5d464ddb056c1d9c4820fba1
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-05-20 11:07:52 -0300

    :recycle: refactor(components/common): refatorar componentes comuns

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,14 +1,43 @@
-import { ActivityIndicator }
-  from 'react-native';
+import {
+  View,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
 
-export default function Loading() {
+import {
+  COLORS,
+  SPACING,
+} from '../../theme';
+
+export default function Loading({
+  size = 'large',
+  color = COLORS.primary,
+  containerStyle,
+  style,
+  ...props
+}) {
   return (
-    <ActivityIndicator
-      size="large"
-      color="#FF0050"
-      style={{
-        marginTop: 50,
-      }}
-    />
+    <View
+      style={[
+        styles.container,
+        containerStyle,
+      ]}
+    >
+      <ActivityIndicator
+        size={size}
+        color={color}
+        style={style}
+        {...props}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: SPACING.xl,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
