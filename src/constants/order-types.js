6605ee7ba9af7dd9898f6630da4d8a4a2fa40a9c
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-05-20 11:04:37 -0300

    :recycle: refactor(constants): desmembrar características das constantes de orders (pedidos)

--- src/constants/order-types.js
+++
@@ -1,5 +0,0 @@
-export const ORDER_TYPE_ICONS = {
-  confecção: 'hanger',
-  modificação: 'content-cut',
-  reparo: 'needle',
-};
--- src/constants/orders/status.js
+++ src/constants/orders/status.js
@@ -1,12 +1,12 @@
 // TODO: Requisitar alteração no BACK para trocar 
-// constantes pelos seguintes valores:
+// constantes de estado de pedido pelos seguintes valores:
 //
 // - Produção (PRODUCTION)
 // - Pronto   (DONE)
 // - Entregue (DELIVERED)
 
 export const ORDER_STATUS = {
-  WAITING: 'Aguardando',
-  PRODUCTION: 'Em andamento',
-  DELIVERED: 'Entregue',
+  WAITING: 'WAITING',
+  PRODUCTION: 'PRODUCTION',
+  DELIVERED: 'DELIVERED',
 };
