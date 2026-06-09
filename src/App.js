0446a05ddb8479cbd4e9d9aae228833a3fe4876d
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-06-09 09:58:22 -0300

    :sparkles: feat(App): adicionar 'SafeAreaView' para evitar que o statusbar do celular ocupe espaço do aplicativo

--- src/App.js
+++ src/App.js
@@ -9,14 +9,20 @@ import {
   useAppFonts,
 } from './hooks/useAppFonts';
 
+import { SafeAreaView } 
+  from 'react-native-safe-area-context';
+
 // TODO: criar contramedidas para caso a API esteja indisponível
 export default function App() {
 
   useAppFonts();
 
   return (
-    <PaperProvider>
-      <AppNavigator />
-    </PaperProvider>
+    <SafeAreaView style={{flex:1}}>
+      <PaperProvider>
+        <AppNavigator />
+      </PaperProvider>
+    </SafeAreaView>
+    
   );
 }
