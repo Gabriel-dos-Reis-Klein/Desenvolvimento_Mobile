78e314afdeb7e141fb9261b3c93bcc840eb17734
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-06-13 17:05:51 -0300

    :lipstick: interface: alterar ícones de pedidos

--- src/theme/orders/type-icons.js
+++ src/theme/orders/type-icons.js
@@ -2,11 +2,11 @@ import { ORDER_TYPES } from '../../constants/orders/types';
 
 export const ORDER_TYPE_ICONS = {
   [ORDER_TYPES.MANUFACTURING]:
-    'hanger',
+    'tag',
 
   [ORDER_TYPES.MODIFICATION]:
-    'content-cut',
+    'scissors',
 
   [ORDER_TYPES.REPAIR]:
-    'needle',
+    'arrows-rotate',
 };
