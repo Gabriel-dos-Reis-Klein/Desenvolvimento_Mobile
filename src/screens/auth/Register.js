447d3bb5f63e60dd063a7d77cf54e84a6c20cbb0
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-06-14 03:18:49 -0300

    :compass: refactor: alterar método de navegação entre telas de autenticação

--- src/screens/auth/Register.js
+++ src/screens/auth/Register.js
@@ -106,7 +106,7 @@ export default function Register({
           <AuthTabs
             activeTab="register"
             onPressLogin={() =>
-              navigation.navigate(
+              navigation.replace(
                 'Login'
               )
             }
